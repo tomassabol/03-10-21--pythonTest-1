3d09d5189a8b8eaa4618bc8813aa123c4751ac7d
Tomáš Sabol <tomassabol2@gmail.com>
2021-03-10 09:50:48 +0100

    Update #1.py

--- A/#1.py
+++ A/#1.py
@@ -4,12 +4,11 @@ list_a = ['A', 'E', 'I', 'O', 'U']
 list_b = ['B', 'M', 'P', 'R', 'S', 'V', 'Z']
 
 with open('password.txt', 'w') as dest_file:
-    password = []
-    for i in range(6):
-        password.append(random.choice(list_b))
-        password.append(random.choice(list_a))
-
-
-print("".join(password)) 
-
-
+    for k in range(5):
+        password = []
+        for i in range(6):
+            password.append(random.choice(list_b))
+            password.append(random.choice(list_a))
+        
+        dest_file.write(str("".join(password)))
+        dest_file.write("\n")
